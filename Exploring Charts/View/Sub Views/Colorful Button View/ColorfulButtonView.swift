cd7ba84684dfd7c7db1dab98506648df711099c9
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-01 14:32:51 +0800

    - added Color Extension

---
+++ Exploring Charts/View/Sub Views/Colorful Button View/ColorfulButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  ColorfulButtonView.swift
+//  Color Extensions
+//
+//  Created by Ronjie Diafante Man-on on 5/1/25.
+//
+
+import SwiftUI
+
+struct ColorfulButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorfulButtonView()
+}
---
+++ Exploring Charts/View/Sub Views/Colorful Button View/RandomArcFromColorListView.swift
@@ -0,0 +1,18 @@
+//
+//  RandomArcFromColorListView.swift
+//  Color Extensions
+//
+//  Created by Ronjie Diafante Man-on on 5/1/25.
+//
+
+import SwiftUI
+
+struct RandomArcFromColorListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RandomArcFromColorListView()
+}
