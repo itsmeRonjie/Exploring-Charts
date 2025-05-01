4827ff48c020ebc5791721d8c470bc8c747973d9
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-01 13:51:58 +0800

    - initialized project

---
+++ Exploring Charts/Exploring_ChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exploring_ChartsApp.swift
+//  Exploring Charts
+//
+//  Created by Ronjie Diafante Man-on on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Exploring_ChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ Exploring Charts/View/MainView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Exploring Charts
+//
+//  Created by Ronjie Diafante Man-on on 5/1/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
