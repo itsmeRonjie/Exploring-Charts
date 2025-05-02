2c4247eda771223ea99f3e01c630729bfc66a3ac
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-02 13:22:05 +0800

    - initialized project

---
+++ Chart Demo 3/Chart/Chart_Demo_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Chart_Demo_3App.swift
+//  Chart Demo 3
+//
+//  Created by Ronjie Diafante Man-on on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Chart_Demo_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
