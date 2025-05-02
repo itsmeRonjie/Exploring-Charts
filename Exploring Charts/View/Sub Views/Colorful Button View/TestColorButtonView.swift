b7c09b819733329f95a220d7d34ace8795c75526
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-02 09:39:36 +0800

    - updated TestColorButtonView - added some 3D Effect

--- Exploring Charts/View/Sub Views/Colorful Button View/TestColorButtonView.swift
+++ Exploring Charts/View/Sub Views/Colorful Button View/TestColorButtonView.swift
@@ -19,7 +19,8 @@ struct TestColorButtonView: View {
         (UIScreen.main.bounds.width / count) - 5
     }
     
- 
+    let threshold: CGFloat = 30
+    
     var body: some View {
         NavigationStack {
             HStack(alignment: .bottom, spacing: 5) {
@@ -29,6 +30,53 @@ struct TestColorButtonView: View {
                         color: color,
                         barWidth: barWidth,
                         height: height
+                    )
+                    .rotation3DEffect(
+                        .degrees(rotateBar ? 180 : 0),
+                        axis: (x: 0, y:1 , z: 0)
+                    )
+
+                }
+            }
+            .rotation3DEffect(
+                .degrees(-tilt * 45),
+                axis: (x: 0, y:1 , z: 0)
+            )
+            .scaleEffect(tilt != 0 ? 0.8 : 1)
+            .gesture(
+                DragGesture()
+                    .onChanged { value in
+                        withAnimation {
+                            if value.translation.width > threshold {
+                                tilt = -1
+                            } else if value.translation.width < -threshold {
+                                tilt = 1
+                            } else {
+                                tilt = 0
+                            }
+                        }
+                    }
+                    .onEnded { value in
+                
+                    }
+            )
+            .onTapGesture {
+                withAnimation {
+                    tilt = 0
+                }
+            }
+            .navigationTitle("Demo")
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    ColorfulButtonView(
+                        colors: $colors,
+                        dim: 50,
+                        offset: 10,
+                        action: {
+                            withAnimation {
+                                rotateBar.toggle()
+                            }
+                        }
                     )
                 }
             }
