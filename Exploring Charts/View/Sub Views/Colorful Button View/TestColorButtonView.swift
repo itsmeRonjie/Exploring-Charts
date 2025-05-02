515f134e18db775d21da0b3f4007dbdb276d80ae
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-02 09:27:51 +0800

    - updated TestColorButtonView

--- Exploring Charts/View/Sub Views/Colorful Button View/TestColorButtonView.swift
+++ Exploring Charts/View/Sub Views/Colorful Button View/TestColorButtonView.swift
@@ -8,11 +8,52 @@
 import SwiftUI
 
 struct TestColorButtonView: View {
+    @State var colors: [Color]
+    @State private var rotateBar: Bool = false
+    @State private var tilt: CGFloat = 0.0
+    
+    var count: CGFloat {
+        CGFloat(colors.count)
+    }
+    var barWidth: CGFloat {
+        (UIScreen.main.bounds.width / count) - 5
+    }
+    
+ 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            HStack(alignment: .bottom, spacing: 5) {
+                ForEach(colors, id: \.self) { color in
+                    let height: Double = Double.random(in: 50...200)
+                    BorderedRectangle(
+                        color: color,
+                        barWidth: barWidth,
+                        height: height
+                    )
+                }
+            }
+        }
+    }
+}
+
+struct BorderedRectangle: View {
+    let color: Color
+    let barWidth: CGFloat
+    let height: CGFloat
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 5)
+            .fill(color)
+            .frame(width: barWidth, height: height)
+            .overlay {
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(lineWidth: 0.7)
+            }
     }
 }
 
 #Preview {
-    TestColorButtonView()
+    TestColorButtonView(
+        colors: Color.defaultColors
+    )
 }
