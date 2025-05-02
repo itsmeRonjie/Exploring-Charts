0faee782929c618f79dd485f0d7ec3767d38c2ee
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-02 10:49:31 +0800

    - Added Charts

--- Chart-Demo-2/Chart/View/Main Views/MainView.swift
+++ Chart-Demo-2/Chart/View/Main Views/MainView.swift
@@ -6,14 +6,63 @@
 //
 
 import SwiftUI
+import Charts
 
 struct MainView: View {
+    let min = 0.0
+    let max = 200.0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Welcome to Charts!")
+                .font(.largeTitle)
+            
+            Chart {
+                BarMark(
+                    x: .value("Day", "Sun"),
+                    y: .value("Sales", 100)
+                )
+                .annotation {
+                    Image(systemName: "figure.walk.circle.fill")
+                        .foregroundStyle(Color.blueViolet)
+                        .fontWeight(.bold)
+                        .font(.title)
+                }
+                
+                BarMark(
+                    x: .value("Day", "Mon"),
+                    y: .value("Sales", 80)
+                )
+                BarMark(
+                    x: .value("Day", "Tue"),
+                    y: .value("Sales", 50)
+                )
+                BarMark(
+                    x: .value("Day", "Wed"),
+                    y: .value("Sales", 75)
+                )
+                
+                BarMark(
+                    x: .value("Day", "Thu"),
+                    y: .value("Sales", 100)
+                )
+                
+                BarMark(
+                    x: .value("Day", "Fri"),
+                    y: .value("Sales", 10)
+                )
+                
+                BarMark(
+                    x: .value("Day", "Sat"),
+                    y: .value("Sales", 50)
+                )
+            }
+            .chartYScale(domain: min...max)
+            .chartXAxis {
+                AxisMarks(position: .bottom)
+            }
+            .chartYAxis {
+                AxisMarks(position: .leading)
+            }
         }
         .padding()
     }
