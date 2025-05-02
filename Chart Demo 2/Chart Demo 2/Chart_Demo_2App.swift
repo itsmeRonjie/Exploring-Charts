efc24e8d212de1519f12925de70671696179763f
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-02 10:24:56 +0800

    - added Chart Demo 2

---
+++ Chart Demo 2/Chart Demo 2/Chart_Demo_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Chart_Demo_2App.swift
+//  Chart Demo 2
+//
+//  Created by Ronjie Diafante Man-on on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Chart_Demo_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
